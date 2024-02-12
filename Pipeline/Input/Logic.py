55bb95f50334b2150a25ceaa541f499882cd3d78
PrathameshP26 <150712231+PrathameshP26@users.noreply.github.com>
2024-02-13 00:27:03 +0530

    modified logic.py

--- Pipeline/Input/Logic.py
+++ Pipeline/Input/Logic.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request,redirect,url_for
 import joblib
+
 app=Flask(__name__)
 
 spam_list=[]
 non_spam_list=[]
+tfidf_model=joblib.load('D:/CDAC_PUNE_PROJECT/CDAC_Group_Project/Pipeline/Input/vectorizer.joblib')
+svm_model = joblib.load('D:/CDAC_PUNE_PROJECT/CDAC_Group_Project/Pipeline/Input/SVM_Classifier_03(RBF_Kernel).joblib')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global text
     if request.method == 'POST':
         text = request.form['text']
-        processed_text = preprocess_text(text)
-        classification = predict(processed_text)
+        classification = classify(text)
         
         if classification == 'spam':
             spam_list.append(text)
@@ -21,22 +24,22 @@ def index():
     else:
         return render_template('Home.html', result=None)
 
-def preprocess_text(text):
-    model=joblib.load('CDAC_Group_Project\Pipeline\Input\tfidf_vectorizer.joblib')
-    model.predict()
-    return text
+def classify(text):
+    # Transform input text using TF-IDF Vectorizer
+    text_vectorized = tfidf_model.transform([text])
+    
+    # Predict using SVM Classifier
+    prediction = svm_model.predict(text_vectorized)
+    
+    # Convert prediction to human-readable form
+    if prediction == 1:
+        return 'spam'
+    else:
+        return 'not spam'
 
-# Placeholder function for the TensorFlow model
-def predict(text):
-    # Your TensorFlow model code here
-    # Placeholder: random prediction for now
-    import random
-    return random.choice(["spam", "not spam"])
-
-
-@app.route('/proceed')
-def proceed():
-    return redirect(url_for('spam_not_spam'),spam_text=spam_list, not_spam_text=non_spam_list)
+@app.route('/spam_not_spam')
+def spam_not_spam():
+    return render_template('spam_not_spam.html',spam_text=spam_list, not_spam_text=non_spam_list)
 
 if __name__=='__main__':
     app.run(debug=True)
