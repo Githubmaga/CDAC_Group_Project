2577adda12d2bb94bd576549b05353bc0979cd1b
PrathameshP26 <150712231+PrathameshP26@users.noreply.github.com>
2024-02-14 22:50:05 +0530

    deleted the sql functions as they are required as of now

--- Pipeline/functions/database_configurations.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def Database_config(app):
-    app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'localhost')
-    app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'root')
-    app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'password')
-    app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'your_database_name')
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_secret_key')
--- Pipeline/functions/drop_table.py
+++
@@ -1,9 +0,0 @@
-from flask_mysqldb import MySQL
-
-# Drop the Database table
-def drop_db(app,mysql):
-    with app.app_context():
-        cursor = mysql.connection.cursor()
-        cursor.execute("DROP TABLE IF EXISTS user_input")
-        mysql.connection.commit()
-        cursor.close()
