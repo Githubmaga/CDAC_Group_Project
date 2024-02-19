b3180f2c6efac8f6fc081f3cd2dd6d6e96ad81fb
PrathameshP26 <150712231+PrathameshP26@users.noreply.github.com>
2024-02-19 16:20:18 +0530

    added the summary and positive and negative sentimantal analysis functions

--- Pipeline/Input/Logic.py
+++ Pipeline/Input/Logic.py
@@ -4,16 +4,52 @@ from gensim.utils import simple_preprocess
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow import keras
 import joblib
+import torch
+from transformers import pipeline
+from sumy.summarizers.lex_rank import LexRankSummarizer
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from transformers import RobertaTokenizer, RobertaForSequenceClassification
+import torch
 
-app=Flask(__name__)
+app=Flask(__name__,static_folder='static')
 
 spam_list=[]
 non_spam_list=[]
-
+positives=[]
+negatives=[]
 
 word2vec_model=Word2Vec.load('D:\CDAC_PUNE_PROJECT\CDAC_Group_Project\Pipeline\Input\word2vec_model.bin')
 
 model = keras.models.load_model('D:\CDAC_PUNE_PROJECT\CDAC_Group_Project\Pipeline\Input\lstm_model4.h5')
+
+'''
+model_path = 'D:\Cdac group project\Pipeline2\Input\pytorchmodel.bin'
+roberta_model = RobertaForSequenceClassification.from_pretrained(model_path)
+tokenizer = RobertaTokenizer.from_pretrained(model_path)
+'''
+
+
+
+def sentiment_analyzer(text):
+    sentimental_model=pipeline('sentiment-analysis')
+    res = sentimental_model(text)
+    sent=res[0].get('label')
+    if sent=='POSITIVE':
+        positives.append(text)
+    else:
+        negatives.append(text)    
+
+def sentiment_analyzer2(text):
+    inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True)
+    outputs = model(**inputs)
+    predicted_class = torch.argmax(outputs.logits).item()
+    sentiment_label = "Positive" if predicted_class == 1 else "Negative"
+    if sentiment_label=='Positive':
+        positives.append(text)
+    else:
+        negatives.append(text)  
+
 
 def clean_text_for_prediction(sent):
     sent_list=simple_preprocess(sent)
@@ -48,7 +84,7 @@ def classify(text):
     prediction = model.predict(padded_sequences)
     
     # Convert prediction to human-readable form
-    if prediction > 0.5:
+    if prediction > 0.9:
         return 'spam'
     else:
         return 'not spam'
@@ -58,9 +94,22 @@ def spam_not_spam():
     return render_template('spam_not_spam.html',spam_text=spam_list, not_spam_text=non_spam_list)
 
 summary_list=[]
-@app.route('/summary')
+@app.route('/summary') 
 def summary():
-    return render_template('summary_pos_neg.html',summary=summary_list)
+    summary_sent=''
+    text=' '.join(non_spam_list)
+    parser = PlaintextParser.from_string(text,Tokenizer('english'))
+    lex_rank_summarizer=LexRankSummarizer()
+    summary=lex_rank_summarizer(parser.document,sentences_count=5)
+    for sent in summary:
+        sent=str(sent)
+        summary_sent=summary_sent + sent
+    
+    #sentiment Analysis
+    for lines in non_spam_list:
+        sentiment_analyzer(lines) 
+    return render_template('summary_pos_neg.html',summary=summary_list,positive_reviews=positives,negative_reviews=negatives,summary_sentences=summary_sent)
+
 
 if __name__=='__main__':
     app.run(debug=True)
